8b748fa4b979871705f04315ad001bef756a581b
kaybeard <kmbrickel@fullsail.edu>
2017-06-02 23:54:07 -0400

    committing for attempt to submit after ticket
    Committing after not being able to access FSO yesterday fingers
    crossed!!

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 var express = require('express');
+var body_parser = require('body-parser');
 var app = express();
 
-var port = 3000;
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({
+    extended: true
+}));
 
-app.get('/', function(req, res) {
-    res.json({healthy: true})
+app.use('/api/v1', require('../src/routes/api.js')(express));
+
+var port = process.env.PORT || 3000;
+
+var server = app.listen(port, function () {
+    console.log('The Server is running on', port);
 });
 
-app.listen(3000, function() {
-    console.log('Server is running on Port:', port,'.');
-});
+module.exports = server;
